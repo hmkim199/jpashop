ed372e24e43842bd351594d7faf8f760787d2500
hmkim199 <hmkim199@gmail.com>
2022-08-22 16:06:10 +0900

    feat: member service

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,19 +1,21 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
-        em.persist(member);
+        em.persist(member); // jpa에서 em.persist(member)를 하면 영속성 컨텍스트에 이 멤버 객체를 올린다! 영속성 컨텍스트는 키와 밸류가 있는데 id값(member_id)이 키가 됨.
     }
 
     public Member findOne(Long id) {
