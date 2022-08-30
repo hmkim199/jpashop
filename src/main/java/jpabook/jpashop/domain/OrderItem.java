fdccf80ed16cf27d79cf697c7709e2179432fa41
hmkim199 <hmkim199@gmail.com>
2022-08-30 17:01:18 +0900

    feat: add 주문 취소, 검색

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -28,9 +31,6 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격. 상품 가격은 바뀔 수 있기 때문에 주문 당시 가격 정보 저장
     private int count; // 주문 수량
-
-    protected OrderItem(){
-    }
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
